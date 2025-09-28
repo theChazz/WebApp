bb0252df18d7669463527f4ecd08a0b004e0e290
theChazz <kasgem@gmail.com>
2025-09-28 21:42:56 +0200

    Add 3 simple test pages without layouts or static file references

---
+++ WebApp/Pages/Test1.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class Test1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApp/Pages/Test3.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class Test3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
