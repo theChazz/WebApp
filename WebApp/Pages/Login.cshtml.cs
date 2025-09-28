89b6c3ba083f3f3859b77aa29c02afd3ad5c0a1f
theChazz <kasgem@gmail.com>
2025-09-28 19:33:44 +0200

    Add AllowAnonymous to Login and Register pages

--- WebApp/Pages/Login.cshtml.cs
+++ WebApp/Pages/Login.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
+    [AllowAnonymous]
     public class LoginModel : PageModel
     {
         public void OnGet()
--- WebApp/Pages/Register.cshtml.cs
+++ WebApp/Pages/Register.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         public void OnGet()
