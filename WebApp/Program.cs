8a5248f8b5ddbbb5411e6d90532a94cc67a264df
theChazz <kasgem@gmail.com>
2025-09-28 21:29:38 +0200

    Revert to default static files configuration

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -24,7 +24,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "wwwroot")) });
+app.UseStaticFiles();
 
 app.UseRouting();
 
